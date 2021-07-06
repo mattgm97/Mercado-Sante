99c9fafb53f5c112515641e5c543f5bc7d2ed4f4
Matheus Germano de Moraes <matheusgermano97@hotmail.com>
2021-07-06 15:58:29 -0300

    o

--- src/js/JS.cbyk.Sa.faleconosco.js
+++ src/js/JS.cbyk.Sa.faleconosco.js
@@ -1,21 +1,126 @@
+$("#cl_home_phone").on("keyup", function(e){
+
+    let tel = $(this).val();
+    tel=tel.replace(/\D/g,"")
+    tel=tel.replace(/^(\d)/,"($1")
+    tel=tel.replace(/(.{3})(\d)/,"$1)$2")
+    if(tel.length == 9) {
+        tel=tel.replace(/(.{1})$/,"-$1")
+    } else if (tel.length == 10) {
+        tel=tel.replace(/(.{2})$/,"-$1")
+    } else if (tel.length == 11) {
+        tel=tel.replace(/(.{3})$/,"-$1")
+    } else if (tel.length == 12) {
+        tel=tel.replace(/(.{4})$/,"-$1")
+    } else if (tel.length > 12) {
+        tel=tel.replace(/(.{4})$/,"-$1")
+    }
+ $(this).val(tel);
+ });
+
+
+ $("#cl_cpf").on("keyup", function(e){
+    $(this).val(
+        $(this).val()
+        .replace(/\D/g,"")
+        .replace(/(\d{3})(\d)/,"$1.$2")
+        .replace(/(\d{3})(\d)/,"$1.$2")
+        .replace(/(\d{3})(\d{1,2})$/,"$1-$2"));
+});
 
 
 
 
 
-function ClientCreate() {
 
-	var cl_name = $("#cl_name").val();
-	var cl_email = $("#cl_email").val();
-	var cl_description = $("#cl_description").val();
 
-	var cl_json = {
-		"email": cl_email,
-		"message": cl_description,
-		"name": cl_name
-	};
+function ContactCreate(storeName, dataEntity, co_client)
+{
+	var co_description 	= $("#co_description").val();
+	var co_type 		= $("#co_type").val();
 
-	var cl_url = "/api/dataentities/FC/documents/";
+	var jsonCO = 	{
+					"client": co_client.replace("CL-",""),
+					"description": co_description,
+					"type": co_type
+					};
+
+	var urlCO = "/api/dataentities/" + dataEntity + "/documents/";
+
+	$.ajax({
+		headers: {
+			"Accept": "application/vnd.vtex.ds.v10+json",
+			"Content-Type": "application/json"
+		},
+		data: JSON.stringify(jsonCO),
+		type: 'PATCH',
+		url: urlCO,
+		success: function (data) {
+		  console.log(data);
+		  ResetMessages()
+		  $("#co_message_success").show();
+		  $("#cl_first_name").val("");
+		  $("#cl_cpf").val("");
+		  $("#cl_email").val("");
+		  $("#cl_home_phone").val("");
+		  $("#co_description").val("");
+		},
+		error: function (data) {
+		  console.log(data);
+		  ResetMessages()
+		  $("#co_message_error").show();
+		}
+	});
+}
+
+function ContactCreateByEmail(storeName, dataEntity, cl_email)
+{
+	/*var cl_url = "/api/dataentities/CL/search/?email=" + cl_email + "&_fields=id";
+	
+	$.ajax({
+		headers: {
+			"Accept": "application/vnd.vtex.ds.v10+json",
+			"Content-Type": "application/json"
+		},
+		type: 'GET',
+		url: cl_url,
+		success: function(data, textStatus, xhr){
+			console.log(data);
+			if(xhr.status == "200" || xhr.status == "201"){
+				ContactCreate(storeName, dataEntity, data[0].id);
+			}else{
+				ResetMessages()
+				$("#co_message_error").show();
+			}
+		},
+		error: function(data){
+			console.log(data);
+			ResetMessages()
+			$("#co_message_error").show();
+		}
+	});*/
+}
+
+function ClientCreate()
+{
+	var storeName		= $("#master_data_store_name").val();
+	var dataEntity		= $("#master_data_data_entity").val();
+
+	var cl_first_name 	= $("#cl_first_name").val();
+	var cl_email 		= $("#cl_email").val();
+	var cl_home_phone 	= $("#cl_home_phone").val();
+    var cl_description  = $("#co_description").val();
+    var cl_cpf          = $("#cl_cpf").val();
+	
+	var cl_json = 	{
+					"firstName": cl_first_name,
+					"email": cl_email,
+					"homePhone": cl_home_phone,
+                    "description": cl_description,
+                    "cpf": cl_cpf
+					};
+
+	var cl_url = "/api/dataentities/CO/documents/";
 
 	$.ajax({
 		headers: {
@@ -25,61 +130,76 @@ function ClientCreate() {
 		data: JSON.stringify(cl_json),
 		type: 'PATCH',
 		url: cl_url,
-		success: function (data, textStatus, xhr) {
+		success: function(data, textStatus, xhr){
 			console.log(data);
-			alert("Formulário enviado!")
-			
+			if(xhr.status == "200" || xhr.status == "201"){
+				ResetMessages()
+				$("#co_message_success").show();
+			}else if(xhr.status == "304"){
+				ContactCreateByEmail(storeName, dataEntity, cl_email);
+			}else{
+				ResetMessages()
+				$("#co_message_error").show();
+			}
 		},
-		error: function (data) {
+		error: function(data){
 			console.log(data);
-			alert("Erro no envio de formulário")
-			
+			ResetMessages()
+			$("#co_message_error").show();
 		}
 	});
 }
 
-
-
-function IsEmail(email) {
-	var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-	return regex.test(email);
+function ResetMessages()
+{
+	$("#co_message_loading").hide();
+	$("#co_message_validate").hide();
+	$("#co_message_success").hide();
+	$("#co_message_error").hide();
 }
 
-function FormValidate() {
+function IsEmail(email) {
+  var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+  return regex.test(email);
+}
+
+function FormValidate()
+{
 	var isFormValidate = true;
 
-	if ($("#cl_name").val() == "") {
+	if($("#cl_first_name").val() == ""){
 		isFormValidate = false;
-		$("#cl_name").focus();
+		$("#cl_first_name").focus();
 	}
-
-	if ((isFormValidate) && ($("#cl_email").val() == "")) {
+	if((isFormValidate) && ($("#cl_email").val() == "")){
 		isFormValidate = false;
 		$("#cl_email").focus();
 	}
-	if ((isFormValidate) && (!IsEmail($("#cl_email").val()))) {
+	if((isFormValidate) && (!IsEmail($("#cl_email").val()))){
 		isFormValidate = false;
 		$("#cl_email").val("");
 		$("#cl_email").focus();
 	}
-	
-	if ((isFormValidate) && ($("#cl_description").val() == "")) {
+	if((isFormValidate) && ($("#cl_home_phone").val() == "")){
 		isFormValidate = false;
-		$("#cl_description").focus();
+		$("#cl_home_phone").focus();
 	}
-
-	if (isFormValidate) {
 	
-		
+	if((isFormValidate) && ($("#co_description").val() == "")){
+		isFormValidate = false;
+		$("#co_description").focus();
+	}
+	
+	if(isFormValidate){
+		ResetMessages()
+		$("#co_message_loading").show();
 		ClientCreate();
-	} else {
-		alert("Formulário preenchido incorretamente! Revise seus dados")
+	}else{
+		ResetMessages()
+		$("#co_message_validate").show();
 	}
 
 	return false;
 }
 
-
-$("#enviarform").click(function(){
-	FormValidate()
-})
+ 
