5712111d7a4e459968844d63528dd2ee2e9e9516
Matheus Germano de Moraes <matheusgermano97@hotmail.com>
2021-07-08 16:48:58 -0300

    o

--- src/js/JS.cbyk.Sa.adicionarprods.js
+++ src/js/JS.cbyk.Sa.adicionarprods.js
@@ -3,6 +3,8 @@ $(".prateleira .btn-add-buy-button-asynchronous").text("ADICIONAR AO CARRINHO")
 }, 1500);
 
 
+
+
 $(document).on('click', '.qtd-shelf .btn', function () {
     var $this = $(this);
     var $qtd = $(this).parent().parent().find('.qtd');
@@ -19,8 +21,19 @@ $(document).on('click', '.qtd-shelf .btn', function () {
 $(".prateleira .link-dinamico").on("click", function(e){
     e.preventDefault();
 
-     let qtd = $(this).parent().find('.qtd-wrapper .qtd-shelf .qtd');
-    let urlbuilder = $(this).parent().find(".btn-add-buy-button-asynchronous").attr('href');
+     let qtd = $(this).parent().parent().find('.qtd-wrapper .qtd-shelf .qtd').val();
+    let urlbuilder = $(this).parent().parent().find(".btn-not-clicked").attr('href');
+    urlbuilder = urlbuilder.replace(/qty=1/g, `qty=${qtd}`)
+    $(this).attr('href', urlbuilder)
     console.log(urlbuilder)
+    let sku = (urlbuilder.split("sku" + '=')[1] || '').split('&')[0];
+    let seller = (urlbuilder.split("seller" + '=')[1] || '').split('&')[0];
+    let item = {
+        id: parseInt(sku),
+        quantity: qtd,
+        seller: seller
+    }
+    console.log(item)
 
+    insertProductOnCart(item);
 })
