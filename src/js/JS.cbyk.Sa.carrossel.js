00f8899f2859b90363cb32cad79b393cec813d39
Matheus Germano de Moraes <matheusgermano97@hotmail.com>
2021-11-23 22:57:26 -0300

    a

--- src/js/JS.cbyk.Sa.carrossel.js
+++ src/js/JS.cbyk.Sa.carrossel.js
@@ -221,13 +221,13 @@ docReady(function() {
 
     $(".slick-prev.slick-arrow").html(`
 <svg xmlns="http://www.w3.org/2000/svg" width="39.523" height="50.523" viewBox="0 0 39.523 50.523">
-  <path fill="none" stroke="rgb(227,80,60)" stroke-linecap="butt" stroke-linejoin="miter" stroke-width="5" d="M27.76157527 11.76157527l-16 14 16 13"/>
+  <path fill="none" stroke="rgb(255,199,1)" stroke-linecap="butt" stroke-linejoin="miter" stroke-width="5" d="M27.76157527 11.76157527l-16 14 16 13"/>
 </svg>
 
 `);
     $(".slick-next.slick-arrow").html(`
 <svg xmlns="http://www.w3.org/2000/svg" width="39.523" height="50.523" viewBox="0 0 39.523 50.523">
-  <path fill="none" stroke="rgb(227,80,60)" stroke-linecap="butt" stroke-linejoin="miter" stroke-width="5" d="M11.7615601 38.76156629l16.00001574-13.99998203-15.9999854-13.00001797"/>
+  <path fill="none" stroke="rgb(255,199,1)" stroke-linecap="butt" stroke-linejoin="miter" stroke-width="5" d="M11.7615601 38.76156629l16.00001574-13.99998203-15.9999854-13.00001797"/>
 </svg>
 
 `);
